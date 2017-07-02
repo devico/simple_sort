6462ba4f85a49dfd74697bf5a9fa74364dee0e48
devico <clamdm@mail.ru>
2017-07-03 01:26:37 +0300

    add specs to column Title

--- tests/portal/spec/models/post_spec.rb
+++ tests/portal/spec/models/post_spec.rb
@@ -1,36 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  before(:each) do
-    3.times { FactoryGirl.create(:post) }
-  end
 
-  it 'has to sort from min to max' do
-    expect(Post.min2max(:id).pluck(:id)).to eq [1, 2, 3]
-  end
-
-  it 'has to sort from max to min' do
-    expect(Post.max2min(:id).pluck(:id)).to eq [3, 2, 1]
-  end
-
-  describe 'Column sorts by simple_sort' do
-    let(:params) { { sort_column: column, sort_type: type_sort } }
-    let(:column) { :id }
-    
-    context 'by ACS' do
-      let(:type_sort) { 'asc' }
-      
-      it 'is valid' do
-        expect(Post.simple_sort(params).pluck(:id)).to eq [3, 2, 1]
-      end
+  describe 'Sort posts using' do  
+    before(:each) do
+      3.times { FactoryGirl.create(:post) }
     end
 
-    context 'by DESC' do
-      let(:type_sort) { 'desc' }
+    it 'method min to max' do
+      expect(Post.min2max(:id).pluck(:id)).to eq [1, 2, 3]
+    end
 
-      it 'is valid' do
-        expect(Post.simple_sort(params).pluck(:id)).to eq [1, 2, 3]
-      end 
+    it 'method max to min' do
+      expect(Post.max2min(:id).pluck(:id)).to eq [3, 2, 1]
+    end
   end
+
+  describe 'Use simple_sort for sorting' do
+    let(:params) { { sort_column: column, sort_type: type_sort } }
+        
+    describe 'column ID' do
+      before(:each) do
+        3.times { FactoryGirl.create(:post) }
+      end
+      let(:column) { :id }
+
+      context ' by ACS' do
+        let(:type_sort) { 'asc' }
+        
+        it 'is valid' do
+          expect(Post.simple_sort(params).pluck(:id)).to eq [3, 2, 1]
+        end
+      end
+
+      context 'by DESC' do
+        let(:type_sort) { 'desc' }
+
+        it 'is valid' do
+          expect(Post.simple_sort(params).pluck(:id)).to eq [1, 2, 3]
+        end 
+      end  
+    end
+
+    describe 'column Title' do
+      before(:each) do
+        FactoryGirl.create(:post, title: 'Вы когда-нибудь получали телеграмму от своего сердца?')
+        FactoryGirl.create(:post, title: 'Как избавиться от пятен…')
+        FactoryGirl.create(:post, title: 'А ваш дом страдает от дефицита картин')
+      end
+      let(:column) { :title }
+
+      context ' by ACS' do
+        let(:type_sort) { 'asc' }
+        
+        it 'is valid' do
+          expect(Post.simple_sort(params).pluck(:title).first).to eq('Как избавиться от пятен…')
+        end
+      end
+
+      context 'by DESC' do
+        let(:type_sort) { 'desc' }
+
+        it 'is valid' do
+          expect(Post.simple_sort(params).pluck(:title).first).to eq('А ваш дом страдает от дефицита картин')
+        end 
+      end  
+    end
   end
 end
--- tests/portal/spec/rails_helper.rb
+++ tests/portal/spec/rails_helper.rb
@@ -1,11 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'capybara/rspec'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'database_cleaner'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -21,7 +24,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -35,26 +38,6 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
- 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
- 
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
- 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
- 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
