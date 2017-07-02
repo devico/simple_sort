5ea735789d2b0bd66657a2923447d7d8d22ce943
devico <clamdm@mail.ru>
2017-07-02 22:09:05 +0300

    add spec for method simple_sort

--- tests/portal/spec/factories/posts.rb
+++ tests/portal/spec/factories/posts.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :post do
     sequence(:title) { |n| "Post #{n}" }
-    #item.body 'and the reason why the data is the same for each instance'
     body { Forgery(:lorem_ipsum).words(5) }
   end
 end
--- tests/portal/spec/models/post_spec.rb
+++ tests/portal/spec/models/post_spec.rb
@@ -12,4 +12,25 @@ RSpec.describe Post, type: :model do
   it 'has to sort from max to min' do
     expect(Post.max2min(:id).pluck(:id)).to eq [3, 2, 1]
   end
+
+  describe 'Column sorts by simple_sort' do
+    let(:params) { { sort_column: column, sort_type: type_sort } }
+    let(:column) { :id }
+    
+    context 'by ACS' do
+      let(:type_sort) { 'asc' }
+      
+      it 'is valid' do
+        expect(Post.simple_sort(params).pluck(:id)).to eq [3, 2, 1]
+      end
+    end
+
+    context 'by DESC' do
+      let(:type_sort) { 'desc' }
+
+      it 'is valid' do
+        expect(Post.simple_sort(params).pluck(:id)).to eq [1, 2, 3]
+      end 
+  end
+  end
 end
